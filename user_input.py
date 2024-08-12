4943eb1fa04d882ed6e8af1588ec0aaed05f8930
austinemark001 <167645394+austinemark001@users.noreply.github.com>
2024-08-12 16:37:35 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+my_name = input('enter your name:\n')
+my_age = input('enter your age: \n')
+my_location = input('enter your location: \n')
+
+print('hello', my_name, ', You are ', my_age, 'years old and live in ', my_location)
